36882138fb5f27496945a3ba099b97d72e5b69a3
daehan-lim <penjan.eng@gmail.com>
2025-04-23 17:18:40 +0900

    add: Add native and target language to User model

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -3,11 +3,15 @@ class User {
   final String name;
   final String? location;
   final String? profileImage;
+  final String? nativeLanguage;
+  final String? targetLanguage;
 
   User({
     required this.id,
     required this.name,
     this.location,
     this.profileImage,
+    this.nativeLanguage,
+    this.targetLanguage,
   });
 }
