a6875dc2f869f104d0a244e9613e56e8061d5c10
daehan-lim <penjan.eng@gmail.com>
2025-04-25 17:45:39 +0900

    refactor(UserListItem): Extract widgets

---
+++ lib/ui/pages/home/tabs/users/widgets/list_text_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ListTextButton extends StatelessWidget {
+  final String text;
+
+  const ListTextButton(this.text, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {},
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 6),
+        backgroundColor: Colors.blue.withValues(alpha: 0.05),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: Size.zero,
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 14.5,
+          color: Colors.blueAccent,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/home/tabs/users/widgets/user_list_item.dart
+++ lib/ui/pages/home/tabs/users/widgets/user_list_item.dart
@@ -4,6 +4,7 @@ import 'package:lang_mate/ui/widgets/app_cached_image.dart';
 
 import '../../../../../../app/constants/app_colors.dart';
 import '../../../../../../data/model/app_user.dart';
+import 'list_text_button.dart';
 
 class UserListItem extends StatelessWidget {
   final AppUser user;
@@ -29,12 +30,6 @@ class UserListItem extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          // CircleAvatar(
-          //   radius: 33,
-          //   backgroundImage: NetworkImage(
-          //     user.profileImage ?? 'https://picsum.photos/200/200?random=1',
-          //   ),
-          // ),
           const SizedBox(width: 12),
 
           // Info column
@@ -42,75 +37,8 @@ class UserListItem extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Name and distance
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // Name
-                          Text(
-                            user.name,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          const SizedBox(height: 4),
-
-                          // Language exchange
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                user.nativeLanguage ?? '',
-                                style: TextStyle(fontSize: 14),
-                              ),
-                              const SizedBox(width: 5),
-                              Icon(
-                                Icons.sync_alt_outlined,
-                                color: Colors.black,
-                                size: 17,
-                              ),
-                              const SizedBox(width: 5),
-                              Text(
-                                user.targetLanguage ?? '',
-                                style: TextStyle(fontSize: 14),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Spacer(),
-                    TextButton(
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 18,
-                          vertical: 6,
-                        ),
-                        backgroundColor: Colors.blue.withValues(alpha: 0.05),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        minimumSize: Size.zero,
-                      ),
-                      child: Text(
-                        '채팅',
-                        style: TextStyle(
-                          fontSize: 14.5,
-                          color: Colors.blueAccent,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
+                // Name, language, chat button
+                _buildTopRow(),
                 const SizedBox(height: 4),
 
                 // District
@@ -151,6 +79,50 @@ class UserListItem extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Row _buildTopRow() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Name
+              Text(
+                user.name,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 4),
+
+              // Language exchange
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    user.nativeLanguage ?? '',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(width: 5),
+                  Icon(Icons.sync_alt_outlined, color: Colors.black, size: 17),
+                  const SizedBox(width: 5),
+                  Text(
+                    user.targetLanguage ?? '',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Spacer(),
+        ListTextButton('채팅'),
+      ],
     );
   }
 }
